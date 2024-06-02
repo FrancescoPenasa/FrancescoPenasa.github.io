49d833c91e87d67cb132521ff8711e9a0f93f181
FrancescoPenasa <francesco.penasa.job@gmail.com>
2024-06-02 20:14:26 +0200

    add auto hidden on main click and add small details

--- src/App.jsx
+++ src/App.jsx
@@ -9,19 +9,25 @@ import Footer from './components/Footer'
 function App() {
   const [count, setCount] = useState(0)
 
+  const [showNavColumn, setShowNavColumn] = useState(false);
+
+  const handleMainClick = () => {
+    setShowNavColumn(false);
+  };
+
   return (
     <>
-    <Nav/>
-      <main className='align-middle justify-center text-slate-400'>
-      <h1 className='align-text-bottom text-black'>Website under construction, in the meantime download my CV</h1>
-      <div className='flex items-center w-full justify-center py-4'>
-        <a href='https://raw.githubusercontent.com/FrancescoPenasa/Awesome-CV/master/en/cv.pdf'> 
-        <Button className="justify-center" children={<p className='text-black font-bold'>Download CV</p>} onClick={console.log("test")}/>        
-        </a>
-      </div>
+      <Nav showNavColumn={showNavColumn} setshowNavColumn={setShowNavColumn} />
+      <main className='align-middle justify-center text-slate-400' onClick={handleMainClick}>
+        <h1 className='align-text-bottom text-black'>Website under construction, in the meantime download my CV</h1>
+        <div className='flex items-center w-full justify-center py-4'>
+          <a href='https://raw.githubusercontent.com/FrancescoPenasa/Awesome-CV/master/en/cv.pdf'> 
+            <Button className="justify-center" children={<p className='text-black font-bold'>Download CV</p>} onClick={console.log("test")}/>        
+          </a>
+        </div>
       </main>
 
-    <Footer/>
+      <Footer/>
     </>
   )
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,9 @@
-import React, {useState} from 'react';
-
-const Nav = () => {
-    const [showNavColumn, setshowNavColumn] = useState(false)
-    
+export default function Nav({ showNavColumn, setshowNavColumn}) {   
     const handleNavColumn = () => {
         setshowNavColumn(!showNavColumn)
     }
     
-    return (
-        <>
+    return <>
         <nav className="fixed left-0 top-0 z-20 mx-auto h-[88px] flex w-full items-center border-b-4 border-black bg-white px-5">
             <div className="mx-auto flex w-[1300px] max-w-full items-center justify-between">
                 {/* nav button */}
@@ -44,9 +39,10 @@ const Nav = () => {
 
 
                 {/* socials */}
-                <div className="flex w-[160px] items-center justify-end gap-5 m800:w-[unset] m400:gap-3">
+                <div className="flex w-[160px] items-center justify-end gap-5 ">
                     {/*  github */}
-                    <a target="_blank" href="https://github.com/FrancescoPenasa" className="flex items-center justify-center rounded-base border-2 border-black p-2 shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] transition-all hover:translate-x-[3px] hover:translate-y-[3px] hover:shadow-none">
+                    <a target="_blank" href="https://github.com/FrancescoPenasa" 
+                    className="flex items-center justify-center rounded-base border-2 border-black p-2 shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] transition-all hover:translate-x-[3px] hover:translate-y-[3px] hover:shadow-none">
                         <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 1024 1024" className="h-6 w-6 m500:h-4 m500:w-4" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M511.6 76.3C264.3 76.2 64 276.4 64 523.5 64 718.9 189.3 885 363.8 946c23.5 5.9 19.9-10.8 19.9-22.2v-77.5c-135.7 15.9-141.2-73.9-150.3-88.9C215 726 171.5 718 184.5 703c30.9-15.9 62.4 4 98.9 57.9 26.4 39.1 77.9 32.5 104 26 5.7-23.5 17.9-44.5 34.7-60.8-140.6-25.2-199.2-111-199.2-213 0-49.5 16.3-95 48.3-131.7-20.4-60.5 1.9-112.3 4.9-120 58.1-5.2 118.5 41.6 123.2 45.3 33-8.9 70.7-13.6 112.9-13.6 42.4 0 80.2 4.9 113.5 13.9 11.3-8.6 67.3-48.8 121.3-43.9 2.9 7.7 24.7 58.3 5.5 118 32.4 36.8 48.9 82.7 48.9 132.3 0 102.2-59 188.1-200 212.9a127.5 127.5 0 0 1 38.1 91v112.5c.8 9 0 17.9 15 17.9 177.1-59.7 304.6-227 304.6-424.1 0-247.2-200.4-447.3-447.5-447.3z"></path></svg>
                     </a>
                         {/* twitter */}
@@ -61,26 +57,31 @@ const Nav = () => {
         {/* pages columns */}
         {showNavColumn ?   
         <aside className="lg:hidden">
-            <div className='fixed left-0 w-[50%] sm:w-[45%] md:w-[40%] bg-white text-black h-full border-r border-r-gray-900'>
-                <ul className='pt-24 uppercase'>
-                    <li className='pi-4'>
-                        <a className="text-xl font-base text-black" href="/#">Home</a>
-                    </li>
-                    <li className='pi-4'>
-                        <a className="text-xl font-base" href="/about">About</a>
-                    </li>
-                    <li className='pi-4'>
-                        <a className="text-xl font-base" href="/projects">Projects</a>
-                    </li>
-                    <li className='pi-4'>
-                        <a className="text-xl font-base" href="/learning">Learning</a>
-                    </li>
+            <div className='fixed left-0 w-[50%] sm:w-[45%] md:w-[40%] bg-yellow-50 text-black h-full border-r border-r-gray-900'>
+                <ul className='pt-12 uppercase m-5 bg'>
+                    <a className="text-xl w-full font-base p-1 items-center" href="/#">
+                        <li className='p-3 hover:bg-slate-600 hover:text-white hover:rounded '>
+                            Home
+                        </li>
+                    </a>
+                    <a className="text-xl w-full font-base p-1" href="/about">
+                        <li className='p-3 hover:bg-slate-600 hover:text-white hover:rounded '>
+                            About
+                        </li>
+                    </a>
+                    <a className="text-xl w-full font-base p-1" href="/projects">
+                        <li className='p-3 hover:bg-slate-600 hover:text-white hover:rounded '>
+                            Projects
+                        </li>
+                    </a>
+                    <a className="text-xl w-full font-base p-1" href="/learning">
+                        <li className='p-3 hover:bg-slate-600 hover:text-white hover:rounded '>
+                            Learning
+                        </li>
+                    </a>
                 </ul>
             </div>
         </aside>
         : null}
         </>
-    );
-};
-
-export default Nav;
+}
