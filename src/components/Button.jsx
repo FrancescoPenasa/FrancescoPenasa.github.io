e8729d48ce6e69578a1b4949b06a0c57f2d15323
FrancescoPenasa <francesco.penasa.job@gmail.com>
2024-06-02 20:12:55 +0200

    fix button shadow

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,7 +5,7 @@ export default function Button({ className, children, onClick }) {
       aria-label="Click to perform an action"
       onClick={onClick}
       className={
-        'flex cursor-pointer items-center rounded-base border-2 border-black bg-main px-4 py-2 text-sm font-base shadow-base transition-all hover:translate-x-boxShadowX hover:translate-y-boxShadowY hover:shadow-none' +  className
+        'flex cursor-pointer items-center rounded-base border-2 border-black bg-main px-4 py-2 text-sm font-base shadow-base transition-all hover:translate-x-boxShadowX hover:translate-y-boxShadowY hover:shadow-none ' +  className
       }
     >
       {children}
