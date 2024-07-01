b76d7daae85256ab5a54c81d6e6c8443efc59c6b
FrancescoPenasa <francesco.penasa.job@gmail.com>
2024-07-01 18:44:55 +0200

    update contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -15,12 +15,8 @@ export default function Contact() {
     <>
       <Nav showNavColumn={showNavColumn} setshowNavColumn={setShowNavColumn} />
       <main className='align-middle justify-center text-slate-400' onClick={handleMainClick}>
-        <h1 className='align-text-bottom text-black'>Hi, you can still download my cv!</h1>
-        <div className='flex items-center w-full justify-center py-4'>
-          <a href='https://raw.githubusercontent.com/FrancescoPenasa/Awesome-CV/master/en/cv.pdf'> 
-            <Button className="justify-center" children={<p className='text-black font-bold'>Download CV</p>} onClick={console.log("test")}/>        
-          </a>
-        </div>
+        <h1 className='align-text-bottom text-black'>Hi, you can find my email and my phone number inside the cv!</h1>
+        <p>Just to avoid email spammer scraper <a href="https://www.matthewthom.as/blog/stop-email-scraping/">Learn more here</a></p>
       </main>
 
       <Footer/>
