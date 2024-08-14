cd1272716f11436bc3fc0fa6a73f15754e140b28
FrancescoPenasa <francesco.penasa.job@gmail.com>
2024-08-14 19:01:27 +0200

    fix footer link

--- src/pages/components/Footer.jsx
+++ src/pages/components/Footer.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
+import { v4 as uuidv4 } from 'uuid';
+
 const Footer = () => {
     return (
         <footer className='w-full bg-slate-100 flex justify-center align-middle space-x-4 '>
             <p>Francesco Penasa © 2024</p> 
             
             {/* <p>View the code on GitHub.</p> */}
-            <a href='/cookies'> <p>Privacy Policy</p></a>
+            <Link key={uuidv4()} className="text-xl font-base" to={"/cookies"}>Privacy Policy</Link>
         </footer>
     );
 };
